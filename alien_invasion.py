fd30783363b847fa5050668eb07f345f33b494b9
duk <dl18595621573@163.com>
2024-06-25 17:16:00 +0800

    提取 run 方法中的代码,提取位辅助方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,18 +22,29 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏主循环"""
         while True:
-            # 监视鼠标事件和键盘事件
-            for event in pygame.event.get():
+            # 鼠标事件和键盘事件
+            self._check_events()
+            # 修改屏幕图像
+            self._update_screen()
+
+    def _check_events(self):
+        """监视鼠标事件和键盘事件"""
+
+        for event in pygame.event.get():
                 # 检测到点击退出按钮
                 if event.type == pygame.QUIT:
                     print("退出成功...")
                     sys.exit()
-                # 重绘屏幕背景色
-                self.screen.fill(self.settings.bg_color)
-                # 让飞船显示在屏幕上
-                self.ship.blitme()
-                # 刷新屏幕可见(擦去旧屏幕 使新屏幕可见)
-                pygame.display.flip()
+    
+    def _update_screen(self):
+        """更新屏幕图像,并使新屏幕可见"""
+
+        # 重绘屏幕背景色
+        self.screen.fill(self.settings.bg_color)
+        # 让飞船显示在屏幕上
+        self.ship.blitme()
+        # 刷新屏幕可见(擦去旧屏幕 使新屏幕可见)
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏并且运行
