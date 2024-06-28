9cbaa97497675c416fe0961243082dccf72192df
duk <dl18595621573@163.com>
2024-06-28 15:52:55 +0800

    让飞船能够发射子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 import settings
 import ship
 import font
+from bullet import Bullet
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -19,6 +20,8 @@ class AlienInvasion:
         self.ship = ship.Ship(self)
         # 初始化字体
         self.font = font.Font(self)
+        # 初始化子弹
+        self.bullets = pygame.sprite.Group()
         # 设置标题
         pygame.display.set_caption("外星人入侵")
 
@@ -31,6 +34,8 @@ class AlienInvasion:
             # 鼠标事件和键盘事件
             self._check_events()
             self.ship.update()
+            # 更新子弹
+            self._update_bullets()
             # 修改屏幕图像
             self._update_screen()
 
@@ -78,7 +83,25 @@ class AlienInvasion:
             self.ship.moveing_down = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        """开火"""
+
+        # 限制子弹数量
+        if len(self.bullets) < self.settings.bullet_allowed:
+            self.bullets.add(Bullet(self))
     
+    def _update_bullets(self):
+        """更新子弹"""
+
+        self.bullets.update()
+        # 超出屏幕 清空子弹
+        for bullet in self.bullets.copy():
+            if bullet.y < 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """更新屏幕图像,并使新屏幕可见"""
 
@@ -87,8 +110,12 @@ class AlienInvasion:
         # 让飞船显示在屏幕上
         self.ship.blitme()
 
+        # 让子弹显示在屏幕上
+        for bullet in self.bullets:
+            bullet.draw_bullet()
+
         # 让文字显示在屏幕上
-        self.font.blitme(self.text)
+        # self.font.blitme(self.text)
 
         # 刷新屏幕可见(擦去旧屏幕 使新屏幕可见)
         pygame.display.flip()
---
+++ bullet.py
@@ -0,0 +1,35 @@
+"""子弹模块"""
+
+from typing import Any
+import pygame
+from pygame.sprite import Sprite
+# from alien_invasion import AlienInvasion
+
+class Bullet(Sprite):
+    """子弹类"""
+
+    def __init__(self, ai_game) -> None:
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # 初始化一个 矩形资源
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        # 将值设置到飞机头部中间
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # 存储子弹
+        self.y = float(self.rect.y) 
+
+    def update(self) -> None:
+        """更新子弹的位置"""
+
+        # 向上移动
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """绘制子弹"""
+
+        pygame.draw.rect(self.screen, self.color, self.rect)
